4c0eae666c1d28df19a4d8aa8f426849b4626087
andre  gitlab <andre.de.ceuninck@gmail.com>
2022-08-02 16:45:18 +0200

    V10.0.3
    update for V10.0.3 usage

--- LDRA/LDRA-instrument-build-exe-analyze.bat
+++ LDRA/LDRA-instrument-build-exe-analyze.bat
@@ -1,8 +1,8 @@
-start /wait C:\LDRA_Toolsuite_C_CPP_10.0.2\Contestbed  C:\Demo\demo-caesar\LDRA\Caesar_V10.tcf -112a345670212q
-cd C:\LDRA_Workarea_C_CPP_10.0.2
+start /wait C:\LDRA_Toolsuite_C_CPP_10.0.3\Contestbed  C:\Demo\demo-caesar\LDRA\Caesar_V10.tcf -112a345670212q
+cd C:\LDRA_Workarea_C_CPP_10.0.3
 C:\Demo\demo-caesar\demo-caesar1.exe
-cd C:\LDRA_Workarea_C_CPP_10.0.2\demo-caesar_tbwrkfls
-start /wait C:\LDRA_Toolsuite_C_CPP_10.0.2\Contestbed  C:\Demo\demo-caesar\LDRA\Caesar_V10.tcf -32panq
-xcopy C:\LDRA_Workarea_C_CPP_10.0.2\demo-caesar_tbwrkfls\demo-caesar_reports C:\Demo\demo-caesar\LDRA\Reports
+cd C:\LDRA_Workarea_C_CPP_10.0.3\demo-caesar_tbwrkfls
+start /wait C:\LDRA_Toolsuite_C_CPP_10.0.3\Contestbed  C:\Demo\demo-caesar\LDRA\Caesar_V10.tcf -32panq
+xcopy C:\LDRA_Workarea_C_CPP_10.0.3\demo-caesar_tbwrkfls\demo-caesar_reports C:\Demo\demo-caesar\LDRA\Reports
 cd C:\Demo\demo-caesar\LDRA\Reports
 "C:\Program Files (x86)\Google\Chrome\Application\chrome.exe" C:\Demo\demo-caesar\LDRA\Reports\demo-caesar.ovs.htm
